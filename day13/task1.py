33a6a76aa4a639fe1da954edbccc1b2d58305f18
ricoandreaslepp <ricoandreaslepp@gmail.com>
2022-12-15 15:00:25 +0200

    need more trues

--- day13/task1.py
+++ day13/task1.py
@@ -14,15 +14,29 @@ else
 """
 # has to be recursive
 # True : "right order", False : "wrong order"
-def comparisons(l1, l2):
-    print(f"comparing {l1} {l2}")
-    for x, y in zip(l1, l2):
+def comparisons(l1, l2):       
+    i = 0
+    while True:
+        # siin probleem
+        if len(l1)==i and len(l2)==i:
+            return None
+
+        if i>=len(l1):
+            return True
+        elif i>=len(l2):
+            return False
+
+        x, y = l1[i], l2[i]
+        print(f"comparing {x} {y}")
         if isinstance(x, int) and isinstance(y, int):
             if x<y:
                 return True
             elif x>y:
                 return False
         elif isinstance(x, list) and isinstance(y, list):
+            if len(x)==0 and len(y)==0:
+                return None
+
             if len(x)==0:
                 return True
             elif len(y)==0:
@@ -35,17 +49,30 @@ def comparisons(l1, l2):
         else:
             # one of them is integer
             # still needs additions
-            if isinstance(l1, list):
-                return comparisons(l1, [l2])
-            elif isinstance(l2, list):
-                return comparisons([l1], l2)
+            if isinstance(x, list):
+                return comparisons(x, [y])
+            elif isinstance(y, list):
+                return comparisons([x], y)
+        
+        i += 1
 
-f = open("test_case")
+i, ans = 1, 0
+f = open("case")
 while True:
     p1 = eval(f.readline().strip())
     p2 = eval(f.readline().strip())
     more = f.readline()
-    print(comparisons(p1, p2))
-
+    if comparisons(p1, p2): 
+        ans += i
+        print("\n")
+    else:
+        print(i)
+        print(p1)
+        print(p2)
+        print("False ^", end="\n\n")
     if not more:
         break
+
+    i += 1
+
+print(ans)
