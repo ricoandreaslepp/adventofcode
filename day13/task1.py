9624c53162052c36f884d15601dd14ab5e159c8c
ricoandreaslepp <ricoandreaslepp@gmail.com>
2022-12-28 19:48:02 +0200

    day13 complete

--- day13/task1.py
+++ day13/task1.py
@@ -1,78 +1,43 @@
 #!/usr/bin/python3
-"""
-if both are ints
-    left<right -> right order
-    right>left -> wrong order
-    else       -> continue
-if both are lists
-    repeat first
-    left runs out  -> right order 
-    right runs out -> wrong order
-    same len       -> continue
-else
-    convert int to list -> repeat snd step
-"""
-# has to be recursive
-# True : "right order", False : "wrong order"
+import json
+
+# 1: right order, -1: wrong order, 0: continue
 def comparisons(l1, l2):       
-    i = 0
-    while True:
-        print(f"comparing {l1} {l2}")
-        # siin probleem
-        if len(l1)==i and len(l2)==i:
-            return None
-
-        if i>=len(l1):
-            return True
-        elif i>=len(l2):
-            return False
-
-        x, y = l1[i], l2[i]
-        if isinstance(x, int) and isinstance(y, int):
-            if x<y:
-                return True
-            elif x>y:
-                return False
-        elif isinstance(x, list) and isinstance(y, list):
-            if len(x)==0 and len(y)==0:
-                return None
-
-            if len(x)==0:
-                return True
-            elif len(y)==0:
-                return False
-            # compare the lists themselves
-            inner = comparisons(x, y)
-            if inner!=None:
-                return inner 
-
-        else:
-            # one of them is integer
-            # still needs additions
-            if isinstance(x, list):
-                return comparisons(x, [y])
-            elif isinstance(y, list):
-                return comparisons([x], y)
+    # both ints
+    if isinstance(l1, int) and isinstance(l2, int):
+        if l1<l2:
+            return 1
+        elif l1>l2:
+            return -1
         
-        i += 1
-
-i, ans = 1, 0
-f = open("case")
-while True:
-    p1 = eval(f.readline().strip())
-    p2 = eval(f.readline().strip())
-    more = f.readline()
-    if comparisons(p1, p2): 
+        return 0
+        
+    # one int
+    if isinstance(l1, int):
+        l1 = [l1]
+    elif isinstance(l2, int):
+        l2 = [l2]
+    
+    # both lists
+    for x, y in zip(l1, l2):
+        pair = comparisons(x, y)
+        if pair==0:
+            continue
+        return pair
+    
+    if len(l1)<len(l2):
+        return 1
+    elif len(l2)<len(l1):
+        return -1
+    
+    return 0
+       
+with open("case") as f:
+    data = f.read().strip()
+         
+ans = 0
+for i, line in enumerate(data.split("\n\n"), start=1):
+    x1, x2 = map(json.loads, line.split("\n"))
+    if comparisons(x1, x2)==1:
         ans += i
-        print("\n")
-    else:
-        print(i)
-        print(p1)
-        print(p2)
-        print("False ^", end="\n\n")
-    if not more:
-        break
-
-    i += 1
-
 print(ans)
--- day13/task2.py
+++ day13/task2.py
@@ -1,1 +1,47 @@
 #!/usr/bin/python3
+import json
+import numpy as np
+from functools import cmp_to_key
+
+# 1: right order, -1: wrong order, 0: continue
+def comparisons(l1, l2):       
+    # both ints
+    if isinstance(l1, int) and isinstance(l2, int):
+        if l1<l2:
+            return 1
+        elif l1>l2:
+            return -1
+        
+        return 0
+        
+    # one int
+    if isinstance(l1, int):
+        l1 = [l1]
+    elif isinstance(l2, int):
+        l2 = [l2]
+    
+    # both lists
+    for x, y in zip(l1, l2):
+        pair = comparisons(x, y)
+        if pair==0:
+            continue
+        return pair
+    
+    if len(l1)<len(l2):
+        return 1
+    elif len(l2)<len(l1):
+        return -1
+    
+    return 0
+       
+with open("case") as f:
+    data = f.read().strip()
+         
+packets = [[], [[2]], [[6]]]
+for line in data.split("\n\n"):
+    for x in map(json.loads, line.split("\n")):
+        packets.append(x)
+
+cmp = cmp_to_key(comparisons)
+packets.sort(key=cmp, reverse=True)
+print(packets.index([[2]])*packets.index([[6]]))
