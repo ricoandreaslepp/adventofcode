208f5d035924bb471740640e14004c6c39877271
ricoandreaslepp <ricoandreaslepp@gmail.com>
2022-12-28 18:45:43 +0200

    day12 - first solution improved

--- day12/task1.py
+++ day12/task1.py
@@ -2,12 +2,10 @@
 from collections import deque
 import numpy as np
 
-def find_and_convert(item, to_what, a):
-    for i in range(len(a)):
-        for j in range(len(a[0])):
-            if a[i][j]==item:
-                a[i][j] = to_what
-                return (i, j)
+def convert(now, then):
+    xs, ys = map(lambda x: int(x[0]), np.where(a==now))
+    a[xs, ys] = then
+    return (xs, ys)
 
 def findShortestPath(a, start, end):
     q = deque([(start, 0)])
@@ -21,19 +19,18 @@ def findShortestPath(a, start, end):
                 
         possible_moves = [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]
         for x, y in possible_moves:
-            if (x, y) not in visited:
-                if x>=0 and x<n and y>=0 and y<m:
-                    if a[x][y]-a[i][j] <= 1:
-                        q.append(((x, y), steps+1))
-                        # before I was generating a huge queue and only then putting these items to the visited list
-                        visited.add((x, y))
+            if x in range(0,n) and y in range(0,m) and all([(x, y) not in visited, ord(a[x,y])-ord(a[i,j]) <= 1]):
+                q.append(((x, y), steps+1))
+                # before I was generating a huge queue and only then putting these items to the visited list
+                visited.add((x, y))
 
 a = []
 for li in open("case"):
-    a.append(list(map(ord, li.strip())))
+    a.append(list(li.strip()))
+a = np.array(a)
 
 n, m = len(a), len(a[0])
-xs, ys = find_and_convert(ord('S'), ord('a'), a)
-xe, ye = find_and_convert(ord('E'), ord('z'), a)
+xs, ys = convert('S', 'a')
+xe, ye = convert('E', 'z')
 
 print(findShortestPath(a, (xs, ys), (xe, ye)))
